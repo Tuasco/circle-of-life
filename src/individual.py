dd26a6637d1c1394e50cfac7da4698d1ed2a450d
Tuasco <ktata.yacine@gmail.com>
2026-01-28 08:53:13 +0100

    Add hunger and starving to individuals

--- src/individual.py
+++ src/individual.py
@@ -32,7 +32,7 @@ class Individual:
         self.energy: int = 100
         
         
-def join_simulation(individual_type: IndividualType, verbose: bool = False) -> tuple[str, str] | None:
+def join_simulation(individual_type: IndividualType, verbose: bool = False) -> tuple[str, ...] | None:
     try:
         with socket(AF_INET, SOCK_STREAM) as client_socket:
             client_socket.settimeout(10)
@@ -56,7 +56,7 @@ def join_simulation(individual_type: IndividualType, verbose: bool = False) -> t
 def main(individual_type: IndividualType, individual_id: int, verbose: bool = False):
     individual = Individual(individual_type)
 
-    # Join the simulation over TCP and get shared memories and id
+    # Join the simulation over TCP and get shared memories
     mem, sem = join_simulation(individual_type, verbose)
     
     # Error or individual_id is None (no slots left)
@@ -73,14 +73,32 @@ def main(individual_type: IndividualType, individual_id: int, verbose: bool = Fa
 
     # Initialize in shared memory (Set Type and Starting Energy)
     type_code = 1 if individual_type.value == "prey" else 2
-    initial_energy = MAX_ENERGY
     with sem:
         mapfile.seek(my_offset)
         # Pack: B=unsigned char (type), i=integer (energy), 3x=padding
-        mapfile.write(struct.pack("=Bi3x", type_code, initial_energy))
+        mapfile.write(struct.pack("=Bi3x", type_code, MAX_ENERGY))
 
-    while True:
-        # TODO add individual logic (probably in functions, one for preys and one for predators)
+    while True:        
+        # By not wrapping everything by the semaphore, we could run into a TOCTOU bug.
+        # Even if we do, it isn't that critical. No one else SHOULD modify our values anyway.
+        current_energy = 0
+        with sem:
+            mapfile.seek(my_offset + 1)
+            current_energy = struct.unpack("=i", mapfile.read(4))[0]
+                    
+        individual.energy = current_energy - randint(MIN_ENERGY_LOSS, MAX_ENERGY_LOSS)
+        if individual.energy <= 0:
+            # Mark as dead in shared memory (type_code 0)
+            with sem:
+                mapfile.seek(my_offset)
+                mapfile.write(struct.pack("=Bi3x", 0, 0))
+            
+            if verbose:
+                print(f"We just died :(")
+            break
+            
+        mapfile.seek(my_offset + 1)
+        mapfile.write(struct.pack("i", individual.energy))
         
         # Finally, wait until the next update
         sleep(randint(MIN_WAIT, MAX_WAIT))
